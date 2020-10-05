8fa4d35e56c597cc9cf1ac92535250042365347c
Óscar Plaza <oscarplate@gmail.com>
2020-10-05 23:05:05 +0200

    transparency on hover, lock transparency onclick (toggle)

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow } = require("electron");
 
 let status = false,
+    transparent_pin = false,
     interval,
     def_min = 25,
     def_sec = 00,
@@ -12,7 +13,7 @@ let min_span = document.getElementById("min"),
     play_but = document.getElementById("play"),
     reload_but = document.getElementById("reload"),
     exit_but = document.getElementById("exit"),
-    eye_img = document.getElementById('eye');
+    eye_img = document.getElementById("eye");
 
 play_but.addEventListener("click", () => {
     if (!status) {
@@ -31,7 +32,7 @@ min_span.addEventListener("change", () => {
     def_min = min_span.value;
 });
 
-min_span.addEventListener('mouseleave', () => {
+min_span.addEventListener("mouseleave", () => {
     play_but.focus();
 });
 
@@ -39,12 +40,25 @@ exit_but.addEventListener("click", () => {
     window.close();
 });
 
+eye_img.addEventListener("click", () => {
+    transparent_pin = !transparent_pin;
+    if (transparent_pin) {
+        eye_img.src = "../img/pushpin.png";
+        document.getElementsByTagName("html")[0].classList.add("transparent");
+    } else {
+        eye_img.src = "../img/eye.png";
+        document.getElementsByTagName("html")[0].classList.remove("transparent");
+    }
+});
+
 eye_img.addEventListener("mouseover", () => {
-    document.getElementsByTagName('html')[0].classList.add('transparent');
+    document.getElementsByTagName("html")[0].classList.add("transparent");
 });
 
 eye_img.addEventListener("mouseleave", () => {
-    document.getElementsByTagName('html')[0].classList.remove('transparent');
+    if (!transparent_pin) {
+        document.getElementsByTagName("html")[0].classList.remove("transparent");
+    }
 });
 
 const play = () => {
