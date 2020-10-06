b570e2934bbe18a2e3f74ce575f31480acabcae9
Óscar Plaza <oscarplate@gmail.com>
2020-10-06 22:28:36 +0200

    trifles

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ function createWindow() {
         icon: __dirname + "./img/tomato.png",
         fullscreenable: false,
         maximizable: false,
+        resizable: false,
         alwaysOnTop: true,
     });
 
