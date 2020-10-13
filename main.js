96026d7dba73ffc0e4b5e10fcb5a9b08eea4a6e2
Óscar Plaza <oscarplate@gmail.com>
2020-10-13 19:42:04 +0200

    package build config

--- main.js
+++ main.js
@@ -24,10 +24,12 @@ function createWindow() {
     //win.webContents.openDevTools({ mode: "detach" });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow);
+if (app.requestSingleInstanceLock()) {
+    // This method will be called when Electron has finished
+    // initialization and is ready to create browser windows.
+    // Some APIs can only be used after this event occurs.
+    app.whenReady().then(createWindow);
+}
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
